8f81f06deb2ff25a6cd48e2fb48ba9abdae10bc4
Alb <sirisywe@gmail.com>
2025-10-30 19:05:32 +0100

    Add initial Plugin class for Deco Limit Lifter

--- src/DecoLimitLifter/Plugin.cs
+++ src/DecoLimitLifter/Plugin.cs
@@ -1,1 +1,17 @@
+using BepInEx;
+using HarmonyLib;
+
+namespace DecoLimitLifter
+{
+    [BepInPlugin("alb.ftd.decolimit", "Deco Limit Lifter", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            var harmony = new Harmony("alb.ftd.decolimit");
+            harmony.PatchAll(typeof(Plugin).Assembly);
+            Logger.LogInfo("Deco Limit Lifter loaded");
+        }
+    }
+}
 
